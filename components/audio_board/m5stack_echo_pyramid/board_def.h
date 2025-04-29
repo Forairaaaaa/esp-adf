3b54640a4b089d2598fabe906bc38cd7d48e25bc
Forairaaaaa <applesyqd@outlook.com>
2025-04-29 16:27:27 +0800

    copy from korvo-2 v3

--- components/audio_board/m5stack_echo_pyramid/board_def.h
+++ components/audio_board/m5stack_echo_pyramid/board_def.h
@@ -46,17 +46,23 @@
 #define ESP_SD_PIN_WP               GPIO_NUM_NC
 
 /**
- * @brief Audio Codec Chip Function Definition
+ * @brief  Audio Codec Chip Function Definition
  */
-#define FUNC_AUDIO_CODEC_EN         (1)
-#define ES8311_MCLK_SOURCE          (-1)   /* 0 From MCLK of esp32   1 From BCLK */
-#define HEADPHONE_DETECT            (-1)
-#define PA_ENABLE_GPIO              GPIO_NUM_NC
-#define CODEC_ADC_I2S_PORT          ((i2s_port_t)0)
-#define CODEC_ADC_BITS_PER_SAMPLE   ((i2s_data_bit_width_t)16) /* 16bit */
-#define CODEC_ADC_SAMPLE_RATE       (48000)
-#define RECORD_HARDWARE_AEC         (true)
-#define BOARD_PA_GAIN               (-1) /* Power amplifier gain defined by board (dB) */
+#define FUNC_AUDIO_CODEC_EN       (1)
+#define CODEC_ADC_I2S_PORT        ((i2s_port_t)0)
+#define CODEC_ADC_BITS_PER_SAMPLE ((i2s_data_bit_width_t)32)  /* 32bit */
+#define CODEC_ADC_SAMPLE_RATE     (48000)
+#define RECORD_HARDWARE_AEC       (true)
+#define BOARD_PA_GAIN             (-1)  /* Power amplifier gain defined by board (dB) */
+#define HEADPHONE_DETECT          (-1)
+#define PA_ENABLE_GPIO            GPIO_NUM_48
+#define ES8311_MCLK_SOURCE        (0)  /* 0 From MCLK of esp32   1 From BCLK */
+#define ES7210_MIC_SELECT         (ES7210_INPUT_MIC1 | ES7210_INPUT_MIC2 | ES7210_INPUT_MIC3)
+
+/**
+ * @brief ADC input data format
+ */
+#define AUDIO_ADC_INPUT_CH_FORMAT "RMNM"
 
 extern audio_hal_func_t AUDIO_CODEC_ES8311_DEFAULT_HANDLE;
 extern audio_hal_func_t AUDIO_CODEC_ES7210_DEFAULT_HANDLE;
@@ -72,11 +78,6 @@ extern audio_hal_func_t AUDIO_CODEC_ES7210_DEFAULT_HANDLE;
             .bits = AUDIO_HAL_BIT_LENGTH_16BITS,        \
         },                                              \
 };
-
-/**
- * @brief ADC input data format
- */
-#define AUDIO_ADC_INPUT_CH_FORMAT "MR"
 
 /**
  * @brief Button Function Definition
